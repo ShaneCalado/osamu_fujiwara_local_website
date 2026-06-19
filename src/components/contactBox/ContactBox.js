6d81c040a0b14b660c444937ec227c12dc8de84c
AbraxasDusk <shane.calado@gmail.com>
2026-06-19 19:16:16 +0900

    error fix from last push

--- src/components/contactBox/ContactBox.js
+++ src/components/contactBox/ContactBox.js
@@ -37,7 +37,7 @@ const ContactBox = forwardRef(({ language }, ref) => {
                 <div className="map-container">
                     <iframe 
                         title="Google Maps Location"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3275.3732138812666!2d135.3408406!3d34.8217078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000f5dea051aeed%3A0x298975073830e9dd!2z6KGM5pS_5pu45aOr6Jek5Y6f5L-u5LqL5YuZ5omA!5e0!3m2!1sen!2sca!4v1781863656440!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3275.3732138812666!2d135.3408406!3d34.8217078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000f5dea051aeed%3A0x298975073830e9dd!2z6KGM5pS_5pu45aOr6Jek5Y6f5L-u5LqL5YuZ5omA!5e0!3m2!1sen!2sca!4v1781863656440!5m2!1sen!2sca" 
                         width="100%" 
                         height="100%" 
                         style={{ border: 0 }} 
